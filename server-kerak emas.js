38314423d7b4010c9fd33afac6671e9a3a27dc4b
Bekzod <bekzodabdupatayev@gmail.com>
2024-11-14 12:24:37 -0800

    fix: fixed web server

---
+++ server-kerak emas.js
@@ -0,0 +1,32 @@
+
+console.log("Web Serverni boshlash")
+
+const express = require("express")
+const app = express();
+const http = require("http")
+
+// 1 - Kirish code
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// 2 - Session code
+
+
+// 3 - Views code
+
+app.set("views", "views");
+app.set("view engine", "ejs")
+
+
+// 4 - Routing code
+app.get("/", function (req, res) {
+res.end("<h1>HELLO WORLD</h1>")
+})
+
+const server = http.createServer(app);
+let PORT = 3001
+server.listen(PORT, function () {
+    console.log(`The server is runing successfully on port ${PORT}`)
+})
