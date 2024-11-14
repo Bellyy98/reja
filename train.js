c97c2deb1cc0ad98d5193e6d5965c4f17060e5ff
Bekzod <bekzodabdupatayev@gmail.com>
2024-11-14 07:35:32 -0800

    fix: fixed packages

--- train.js
+++ train.js
@@ -1,1 +1,9 @@
 console.log("train task ishga tushdi");
+
+const moment = require("moment")
+const time = moment().format()
+console.log(time) 
+
+
+const cpuInstances = require("os").cpus();
+console.log(cpuInstances.length)
