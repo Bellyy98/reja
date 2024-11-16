89dbdafdb6c4c35a493b14d836d2c05c29b6d81a
Bekzod <bekzodabdupatayev@gmail.com>
2024-11-16 07:42:57 -0800

    feat: develop author router ejs page

--- server-kerak emas.js
+++ server-kerak emas.js
@@ -1,32 +1,32 @@
 
-console.log("Web Serverni boshlash")
+// console.log("Web Serverni boshlash")
 
-const express = require("express")
-const app = express();
-const http = require("http")
+// const express = require("express")
+// const app = express();
+// const http = require("http")
 
-// 1 - Kirish code
+// // 1 - Kirish code
 
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+// app.use(express.static("public"));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
 
-// 2 - Session code
-
-
-// 3 - Views code
-
-app.set("views", "views");
-app.set("view engine", "ejs")
+// // 2 - Session code
 
 
-// 4 - Routing code
-app.get("/", function (req, res) {
-res.end("<h1>HELLO WORLD</h1>")
-})
+// // 3 - Views code
 
-const server = http.createServer(app);
-let PORT = 3001
-server.listen(PORT, function () {
-    console.log(`The server is runing successfully on port ${PORT}`)
-})
+// app.set("views", "views");
+// app.set("view engine", "ejs")
+
+
+// // 4 - Routing code
+// app.get("/", function (req, res) {
+// res.end("<h1>HELLO WORLD</h1>")
+// })
+
+// const server = http.createServer(app);
+// let PORT = 3001
+// server.listen(PORT, function () {
+//     console.log(`The server is runing successfully on port ${PORT}`)
+// })
